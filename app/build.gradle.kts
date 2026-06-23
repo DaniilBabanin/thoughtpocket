48275a411c331cc2dbefd6b3b36eaf52481f29e6
Daniil Babanin <daniil@babanin.de>
2026-06-23 19:46:38 +0200

    Bump to 0.1.5-pre (versionCode 6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.thoughtpocket"
         minSdk = 31
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.1.4-pre"
+        versionCode = 6
+        versionName = "0.1.5-pre"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
