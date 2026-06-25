50863c4ff9947ca9cc400e30e858197c0624ef20
Daniil Babanin <daniil@babanin.de>
2026-06-25 20:30:55 +0200

    Bump to 0.1.6-pre (versionCode 7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.thoughtpocket"
         minSdk = 31
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.1.5-pre"
+        versionCode = 7
+        versionName = "0.1.6-pre"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
